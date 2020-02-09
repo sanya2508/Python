74bd6d8dfe386268928e9991ce0c9c65c1077ac1
Sanya Kumari <55986045+sanya2508@users.noreply.github.com>
2020-02-09 12:04:15 +0530

    Create abstract4.py

---
+++ abstract4.py
@@ -0,0 +1,44 @@
+#Tuple: immutable list (list that can't be modified)
+
+#count()	Returns the number of times a specified value occurs in a tuple
+
+#index()	Searches the tuple for a specified value and returns the position of where it was found
+
+
+#set
+#unordered collection of unique objects
+
+#add()	Adds an element to the set
+
+#clear()	Removes all the elements from the set
+
+#copy()	Returns a copy of the set
+
+#difference()	Returns a set containing the difference between two or more sets
+
+#difference_update()	Removes the items in this set that are also included in another, specified set
+
+#discard()	Remove the specified item
+
+#intersection()	Returns a set, that is the intersection of two other sets
+
+#intersection_update()	Removes the items in this set that are not present in other, specified set(s)
+
+#isdisjoint()	Returns whether two sets have a intersection or not
+
+#issubset()	Returns whether another set contains this set or not
+
+#issuperset()	Returns whether this set contains another set or not
+
+#pop()	Removes an element from the set
+
+#remove()	Removes the specified element
+
+#symmetric_difference()	Returns a set with the symmetric differences of two sets
+
+#symmetric_difference_update()	inserts the symmetric differences from this set and another
+
+#union()	Return a set containing the union of sets
+
+#update()	Update the set with the union of this set and others
+
