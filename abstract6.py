c64e5f79173d20e6ea0dbfc1f9e914aebe971afa
Sanya Kumari <55986045+sanya2508@users.noreply.github.com>
2020-02-10 19:22:26 +0530

    Create abstract6.py

---
+++ abstract6.py
@@ -0,0 +1,29 @@
+#Functions
+
+def say_hello():
+  print ('helllloo')
+
+say_hello()
+
+#parameters
+def say_hello(name, emoji):
+  print (f'helllloo {name} {emoji}')
+
+#arguments
+say_hello('sanya', ':D') #call, invoke
+
+#positional arguments: arguments req to be in proper position (above)
+
+#keyword arguments:
+say_hello(emoji=':P', name='sanyak')
+
+
+
+#default parameters
+#def say_bye(name='sanya', emoji=';)'):
+ # print (f'helllloo {name} {emoji}')
+
+  #say_bye()
+  #say_bye('abc', ':)')
+
+  
